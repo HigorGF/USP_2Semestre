989031e4dfd0e0c766dd0e2288b14f2791e9dcbf
Higor Freitas <58052097+HigorGF@users.noreply.github.com>
2024-08-29 21:47:24 -0300

    Aula 8

---
+++ AED I/Aula8.c
@@ -0,0 +1,42 @@
+// 2 EPs
+    //Lista ligada ordenada
+    //Nó Cabeça
+    //Lista duplamente ligada
+//Entrega dia 19/10
+
+
+//Continuação Pilhas
+//As aulas anteriores eram pilhas estáticas
+//Dinamica, alocação de memória acontecerá conforme a necessidade~
+//Não existe pilha dupla aqui em um lugar (que nem a na aula passada), como eu já estou economizando
+//memoria eu posso simplesmente criar uma segunda
+
+
+//Diferença da lista ligada
+    //A estrutura em sí não é tão diferente, o que muda de fato é a maneira que
+    //a inserção a exlcusão e a busca funcionam.
+    //Pilhas sempre não possuem ordenção, listas ligadas podem possuir
+    
+
+    //Saber se está vazio também é diferente se saber se o numero de elementos está igual a 0
+        //Isso pode ser util, eu posso ver se está vazio sem fazer toda conta
+
+//Conferir numero de elementos
+    //*não anotei*
+
+
+//Ver se está vazia
+    //Báscio, só procurar a primeira prova
+    int verificar(pilha* p){
+        if(!p->topo) return true;
+        return false; //achou alguma coisa
+    }
+    //Ai fica O(1) invés O(N) como seria a contagem de elementos
+
+    
+
+
+
+
+
+
