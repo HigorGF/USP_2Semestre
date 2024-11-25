7c31587b4d3b7e0c794c80d00bd03b7bc30e809a
Higor Freitas <silhigor.freitas@gmail.com>
2024-11-25 20:39:06 -0300

    27

---
+++ AED I/Aula27.c
@@ -0,0 +1,21 @@
+//AED
+
+//Correção do EP
+
+//Continuando Arvores AVL
+    //Rotação Está pronta
+    //Agora dá para fazer o código da inserção.
+
+    //No geral é recursiva, e sempre será no máx uma rotação na inclusão  para deixar a arvore balanceada
+
+
+//Ainda temos que provar que a altura será propocional a log2 n nós.
+
+
+//A unica que consegue fazer inserção exclusão e busca em logn até então
+
+//Exclusão: o alterou pode descer como true na recurisividade, pois ela pode fazer várias rotações para deixar a ávore balanceada enquanto vai "Subindo".
+
+
+
+
